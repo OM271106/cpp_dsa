840fffeffb3b0f832560b28572f02db5b1d2e476
OM271106 <omdwivedi41@gmail.com>
2026-05-05 20:34:31 +0530

    created map hashing

---
+++ cpp_hashing/map_hashing.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+int n;
+cin >> n;
+int arr[n];
+map<int , int>mpp;
+for(int i=0;i<n;i++){
+    cin>>arr[i];
+    mpp[arr[i]]++;
+}
+
+int q ;
+cin >> q;
+while(q--){
+    int numbers;
+    cin>>numbers;
+    //fetch
+    cout << mpp[numbers] <<endl;
+}
+
+
+
+}
