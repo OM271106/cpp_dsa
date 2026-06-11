b7347542035823a3d5cac3464d8f30d6f733b759
OM271106 <omdwivedi41@gmail.com>
2026-06-11 11:40:00 +0530

    created maximum no of one's

---
+++ cpp_Array/Max_Consecutive_Ones.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count ;
+        count =0;
+        int Maxcount ;
+        Maxcount =0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==1){
+                count=count+1;
+            
+            if(count>Maxcount){
+                Maxcount=count;
+            }
+        }
+        else{
+           count=0;
+        }
+    }
+     return Maxcount;
+}
+};
+int main() {
+    // Magic file routing
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+   // freopen("input.txt", "r", stdin);
+   // freopen("output.txt", "w", stdout);
+#endif
+
+    // Read the array size from input.txt
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    
+    // Read the array numbers from input.txt
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+ 
+    // ------------------------------------------
+    // 3. TEST YOUR LOGIC HERE
+    // ------------------------------------------
+    Solution mySolution;             // Create your tool
+   int answer=mySolution.findMaxConsecutiveOnes(arr);// Run your tool
+    
+    // Print the result to output.txt
+cout << " " << answer<< "\n";
+    return 0;
+}
